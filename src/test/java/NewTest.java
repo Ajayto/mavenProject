0e9c0ce69c509ba5ef277f56a5a8b135532eacf1
unknown <ajaytoradmal2009@gmail.com>
2024-09-30 23:35:57 +0530

    maven project

---
+++ src/test/java/NewTest.java
@@ -0,0 +1,35 @@
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeSuite;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  
+	  WebDriver driver= new ChromeDriver();
+	  driver.get("https://help.blazemeter.com/");
+	  System.out.println("new test");
+  }
+  
+  @Test
+public void f1() {
+	  
+	  WebDriver driver= new ChromeDriver();
+	  driver.get("https://help.blazemeter.com/");
+	  System.out.println("new test");
+  }
+  @Test
+public void f2() {
+	  
+	  WebDriver driver= new ChromeDriver();
+	  driver.get("https://help.blazemeter.com/");
+	  System.out.println("new test");
+}
+
+  @BeforeSuite
+  public void beforeSuite() {
+	  System.out.println("BeforeSuite");
+  }
+
+}
